c129435f800e413f5dcbda449168db97919b4681
Viswa Teja <115293958+v-teja@users.noreply.github.com>
2023-03-18 14:48:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 function calculateMinCost() {
-  //your code here
-  
-  
-  
+  var inputData = document.querySelector("input").value;
+var inputArray = inputData.split(",");
+
+	for(let i=0;i<inputArray.length;i++){
+		inputArray[i] = Number(inputArray[i]);
+	}
+	var cost =0;
+
+	while(inputArray.length>1){
+		inputArray.sort(function (a,b){return a-b;});
+		var newRope = inputArray[0]+inputArray[1];
+		cost+=newRope;
+		inputArray.splice(0,2);
+		inputArray.push(newRope);
+	}
+	//alert(cost);
+	document.querySelector("#result").textContent = cost;
 }  
+
